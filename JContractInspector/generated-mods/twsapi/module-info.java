666995f066f8566a9fdb323e0654d4eaec8dc0e9
brian <brian@i3>
2018-06-30 15:52:35 -0400

    started #1

---
+++ JContractInspector/generated-mods/twsapi/module-info.java
@@ -0,0 +1,6 @@
+module twsapi {
+    exports com.ib.client;
+    exports com.ib.contracts;
+    exports com.ib.controller;
+
+}
--- JContractInspector/src/jcontractinspector/JContractInspector.java
+++ JContractInspector/src/jcontractinspector/JContractInspector.java
@@ -1,6 +1,12 @@
 package jcontractinspector;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,6 +19,10 @@ import javafx.stage.Stage;
  */
 public class JContractInspector extends Application {
     private FormCtlr ctlr;
+    private final Properties config = new Properties();
+    private Stage stage;
+    private final String CONFIG = this.getClass().getSimpleName() + ".properties";
+    private double x,y; //remember for closing iconified
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -27,23 +37,39 @@ public class JContractInspector extends Application {
             scene.getStylesheets().add(css);
         }
 //        scene.getStylesheets().add("jcontractinspector/form.css");//too hard coded
+
+        File propFile = new File(CONFIG);
+        propFile.createNewFile();
+        config.load(new FileInputStream(propFile));
+        stage.setX(Math.max(0, Double.parseDouble(config.getProperty("screenX","0"))));
+        stage.setY(Math.max(0, Double.parseDouble(config.getProperty("screenY","0"))));
+        stage.setWidth(Double.parseDouble(config.getProperty("screenW","1200")));
+        stage.setHeight(Double.parseDouble(config.getProperty("screenH","800")));
         
         stage.setTitle(this.getClass().getSimpleName());
         stage.setScene(scene);
         stage.show();
+        
+        stage.xProperty().addListener((obs,ov,nv) -> {
+            if (nv.doubleValue() > 0) x = nv.doubleValue();//compare to desktop
+        });
+        stage.yProperty().addListener((obs,ov,nv) -> {
+            if (nv.doubleValue() > 0) y = nv.doubleValue();//compare to desktop
+        });
+        
+        this.stage = stage;
     }
 
     @Override public void stop() {
-//        config.setProperty("iconified", String.format("%b", stage.isIconified()));
-//        stage.setIconified(false);
-//        config.setProperty("screenX", stage.getX());
-//        config.setProperty("screenY", String.format("%.1f", stage.getY()));
-//        config.setProperty("screenW", String.format("%.1f", stage.getWidth()));
-//        config.setProperty("screenH", String.format("%.1f", stage.getHeight()));
-//        try (OutputStream out = new FileOutputStream(new File("JContract.properties"))) {
-//            config.store(out, "JContract properties");
-//        } catch (IOException ex) {}
-//        
+        System.out.println(x+ " " +y);
+        config.setProperty("screenX", String.format("%.1f", x));
+        config.setProperty("screenY", String.format("%.1f", y));
+        config.setProperty("screenW", String.format("%.1f", Math.max(500, stage.getWidth())));
+        config.setProperty("screenH", String.format("%.1f", Math.max(500, stage.getHeight())));
+        try (OutputStream out = new FileOutputStream(new File(CONFIG))) {
+            config.store(out, "no comment");
+        } catch (IOException ex) {}
+        
         ctlr.disconnect();
     }
     
